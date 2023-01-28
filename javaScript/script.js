a33e5d966ae7e26b5dc0013c84e3519cf0926365
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-01-28 16:24:46 -0700

    added api key and api query

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,25 @@
-var apiKey = "ea3ee96ce7a370c560d2a99d58278202";
-var today = moment().format('L');
-var historyList = [];
+const apiKey = "d9c88807aaee92c5216a32bb98251ad2";
+const today = moment().format('L');
+const historyList = [];
+
+const currentWeather = (city) => {
+
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(cityWeatherResult) {
+        console.log(cityWeatherResult);
+
+        $("#weatherContent").css("display", "block");
+        $("#cityDetail").empty();
+
+    })
+
+    $("#searchBtn").on("click", function(event) {
+        event.preventDefault();
+
+        var city = $("enterCity").val().trim();
+    })
+}
