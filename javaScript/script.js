38d74555862169dcfb905588a78cb29a6cb9d67c
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-01-29 16:26:14 -0700

    added five day forecast cards and updated styling

--- javaScript/script.js
+++ javaScript/script.js
@@ -17,6 +17,8 @@ const currentWeather = (city) => {
 
         const iconCode = cityWeatherResult.weather[0].icon;
         const iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+        const lat = cityWeatherResult.coord.lat;
+        const lon = cityWeatherResult.coord.lon;
 
         const currentCity = $(`
             <h2 id="currentCity">
@@ -25,10 +27,54 @@ const currentWeather = (city) => {
             <p>Temp: ${cityWeatherResult.main.temp} </p>
             <p>Humidity: ${cityWeatherResult.main.humidity} </p>
             <p>Wind Speed: ${cityWeatherResult.wind.speed} </p>
+            </div>
             `)
+            
         $("#cityDetail").append(currentCity);
-  
+
+        fiveDayForecast(lat, lon);
     });
+}
+
+const fiveDayForecast = (lat,lon) => {
+    const forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    }).then(function(forecastResult) {
+        console.log(forecastResult);
+        $("#fiveDay").empty();
+
+        for (let i = 1; i < 6; i++) {
+            const cityInfo = {
+                date: forecastResult.daily[i].dt,
+                icon: forecastResult.daily[i].weather[0].icon,
+                temp: forecastResult.daily[i].temp.day,
+                humidity: forecastResult.daily[i].humidity,
+            };
+
+            const date = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            const iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${forecastResult.daily[i].weather[0].main}" />`;
+
+            const fiveDayForecastCard = $(`
+                <div class="ml-5 pl-5">
+                    <div class= "card text-white bg-primary mb-3" style="width: 18rem;">
+                    <div class="card-header text-center">
+                        <h2>${date}</h2>
+                    </div>
+                         <div class="card-body text-center">
+                            <p>${iconURL}</p>
+                            <h3>Temp: ${cityInfo.temp} </h3>
+                            <h3>Humidity: ${cityInfo.humidity}</h3>
+                         </div>
+                    </div>
+                </div>
+        `)
+        $("#fiveDay").append(fiveDayForecastCard);
+            
+        }
+    })
 }
 
 $("#searchBtn").on("click", function(event) {
